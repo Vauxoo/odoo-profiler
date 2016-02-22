4a505f0b8f64e30fc675ca0245c863bfba8c63c9
Moisés López <moylop260@vauxoo.com>
2016-02-22 16:03:13 +0000

    [REF] profiler: Add double profile to orm methods

--- profiler/__init__.py
+++ profiler/__init__.py
@@ -2,7 +2,7 @@ from . import controllers  # noqa
 import openerp
 from cProfile import Profile
 from . import core
-from .core import profiling
+from .core import profiling, profiling_orm_methods  # noqa
 from openerp.addons.web.http import JsonRequest
 from openerp.service.server import ThreadedServer
 import os
@@ -34,6 +34,8 @@ def dump_stats():
     """Dump stats to standard file"""
     _logger.info('Dump stats')
     core.profile.dump_stats(os.path.expanduser('~/.openerp_server.stats'))
+    core.profile_orm_methods.dump_stats(
+        os.path.expanduser('~/.openerp_server_orm_methods.stats'))
 
 
 def patch_orm_methods():
@@ -43,17 +45,20 @@ def patch_orm_methods():
     # TODO: Apply a monkey patch of nested method.
     fname_to_patch = os.path.join(os.path.dirname(openerp.api.__file__),
                                   "api.py")
-    odoo_is_patched = 'with profiling():' in open(fname_to_patch).read()
+    odoo_is_patched = 'with profiling_orm_methods():' in \
+        open(fname_to_patch).read()
     if odoo_is_patched:
         _logger.info('The method openerp.api.make_wrapper is patching!')
         return True
     patch_cmds = [
-        'sed -i "s/return new_api/with profiling(): return new_api/g" ' +
+        'sed -i "s/return new_api/'
+        'with profiling_orm_methods(): return new_api/g" ' +
         fname_to_patch,
-        'sed -i "s/return old_api/with profiling(): return old_api/g" ' +
+        'sed -i "s/return old_api/'
+        'with profiling_orm_methods(): return old_api/g" ' +
         fname_to_patch,
         'sed -i "/# avoid hasattr/a ' + '\\ \\ \\ \\ \\ \\ \\ \\ '
-        'from openerp.addons.profiler import profiling" ' +
+        'from openerp.addons.profiler import profiling_orm_methods" ' +
         fname_to_patch,
     ]
     _logger.warn('You will need apply a manual patch to odoo.api.make_wrapper'
@@ -66,6 +71,12 @@ def create_profile():
     """Create the global, shared profile object."""
     _logger.info('Create core.profile')
     core.profile = Profile()
+
+
+def create_profile_orm_methods():
+    """Create other global, shared profile object."""
+    _logger.info('Create core.profile_orm_methods')
+    core.profile_orm_methods = Profile()
 
 
 def patch_stop():
@@ -89,5 +100,6 @@ def post_load():
         # Enable profile in test mode for orm methods.
         _logger.info('Enabling core and apply patch')
         core.enabled = True
+        create_profile_orm_methods()
         patch_orm_methods()
         patch_stop()
