a47c775469a65c7a0778efa9c8ac62d2d23c34ff
Moisés López <moylop260@vauxoo.com>
2017-08-07 19:36:24 -0500

    Revert "[IMP] profiler: Patching odoo connection to process PGOPTIONS (#28)"
    This reverts commit 661a081aa7baec82d47bf14431fbc8297db7d000.
    
    Now odoo>=9.0 don't allow use DSN https://github.com/odoo/odoo/pull/18060

--- profiler/hooks.py
+++ profiler/hooks.py
@@ -6,13 +6,10 @@ import logging
 import os
 from contextlib import contextmanager
 from cProfile import Profile
-
 import openerp
 
 from openerp.http import WebRequest
 from openerp.service.server import ThreadedServer
-from openerp import sql_db
-
 _logger = logging.getLogger(__name__)
 
 
@@ -43,9 +40,6 @@ def patch_odoo():
     execution.
 
     For instance, Odoo spawns a new thread for each request.
-
-    Modify database connect method to add options to enable postgresql logging
-    based on PGOPTIONS environment variable
     """
     _logger.info('Patching openerp.http.WebRequest._call_function')
     webreq_f_origin = WebRequest._call_function
@@ -54,16 +48,6 @@ def patch_odoo():
         with profiling():
             return webreq_f_origin(*args, **kwargs)
     WebRequest._call_function = webreq_f
-
-    _logger.info('Patching openerp.sql_db.db_connect')
-    db_connect_origin = sql_db.db_connect
-    def dbconnect_f(to, *args, **kwargs):
-        try:
-            to += " options='%s' " % (os.environ['PGOPTIONS'] or '')
-        except KeyError:
-            pass
-        return db_connect_origin(to, *args, **kwargs)
-    sql_db.db_connect = dbconnect_f
 
 
 def dump_stats():
