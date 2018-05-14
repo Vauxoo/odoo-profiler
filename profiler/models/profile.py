2ea1d5346f7bd1ef730da1f49998cc5bc72987aa
Moisés López <moylop260@vauxoo.com>
2018-05-13 23:08:04 -0500

    working buttons

--- profiler/models/profile.py
+++ profiler/models/profile.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields
+from openerp import api, fields, models
 
 
 class ProfilerProfile(models.Model):
@@ -10,13 +10,21 @@ class ProfilerProfile(models.Model):
     enable_python = fields.Boolean()
     date_started = fields.Char(readonly=True)
     date_finished = fields.Char(readonly=True)
+    state = fields.Selection([
+        ('enabled', 'Enabled'),
+        ('disabled', 'Disabled'),
+    ], default='disabled', readonly=True)
 
+    @api.multi
     def enable(self):
         self.write(dict(
-            date_started=fields.Datetime.now
+            date_started=fields.Datetime.now(),
+            state='enabled'
         ))
 
+    @api.multi
     def disable(self):
         self.write(dict(
-            date_finished=fields.Datetime.now
+            date_finished=fields.Datetime.now(),
+            state='disabled'
         ))
